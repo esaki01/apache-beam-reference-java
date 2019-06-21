121a72a0acb07c11c1e41af92ca1aea2ba397296
esaki01 <esaki_koki@cyberagent.co.jp>
2019-06-21 11:16:14 +0900

    カスタムオプションを実装

--- src/main/java/org/apache/beam/examples/CoreTransforms/ParDo/Main.java
+++ src/main/java/org/apache/beam/examples/CoreTransforms/ParDo/Main.java
@@ -1,14 +1,27 @@
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 
-import java.util.Arrays;
-
 public class Main {
+    // カスタムオプション
+    public interface MyOptions extends PipelineOptions {
+        @Description("Input for the pipeline")
+        @Default.String("gs://my-bucket/input")
+        String getInput();
+        void setInput(String input);
+
+        @Description("Output for the pipeline")
+        @Default.String("gs://my-bucket/input")
+        String getOutput();
+        void setOutput(String output);
+    }
+
+    // 文字数を求めるDoFnのサブクラス. 詳細は次の記事を参照.
     static class ComputeWordLengthFn extends DoFn<String, String> {
         @ProcessElement
         public void processElement(@Element String word, OutputReceiver<String> out) {
@@ -17,13 +30,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        PipelineOptions options = PipelineOptionsFactory.create();
+        PipelineOptionsFactory.register(MyOptions.class);
+        MyOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(MyOptions.class);
 
         Pipeline p = Pipeline.create(options);
 
-        p.apply(Create.of(Arrays.asList("good morning.", "good afternoon.", "good evening.")))
-                .apply(ParDo.of(new ComputeWordLengthFn()))
-                .apply(TextIO.write().to("src/main/java/org/apache/beam/examples/CoreTransforms/ParDo/output").withSuffix(".txt"));
+        p.apply("ReadFromText", TextIO.read().from(options.getInput())) // I/O Transformを適用してオプションで指定したパスにデータを読み込む
+                .apply(ParDo.of(new ComputeWordLengthFn())) // ParDo Transformを適用（詳細は次の記事を参照）
+                .apply(TextIO.write().to(options.getOutput())); // I/O Transformを適用してオプションで指定したパスにデータを書き込む
 
         p.run().waitUntilFinish();
     }
